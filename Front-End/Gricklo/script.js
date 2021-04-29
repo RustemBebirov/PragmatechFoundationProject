23ed9a8ae002bc00ae934e582852724a11c000ec
RustemBebirov <80380074+RustemBebirov@users.noreply.github.com>
2021-04-29 20:08:18 +0400

    script..js

--- Front-End/Gricklo/script.js
+++ Front-End/Gricklo/script.js
@@ -3,7 +3,7 @@ let sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.next').addEventListener('click', () => {
     move += 480;
-    if (move >= 1900) {
+    if (move >= 1920) {
         move = 0
 
     }
@@ -25,13 +25,12 @@ function autoplay(params) {
     let sliderLine2 = document.querySelector('.slider-line-2');
     sliderLine2.style.left = -575 + 'px'
 
-
-
 }
 
-setTimeout(() => {
+setInterval(() => {
     autoplay()
 }, 1000);
+
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > window.innerHeight / 4 - document.querySelector('header').clientHeight) {
@@ -40,4 +39,37 @@ window.addEventListener('scroll', () => {
         document.querySelector('.main-header').classList.remove('sticky-bar')
     }
 
+})
+
+
+let div = document.createElement('div');
+div.setAttribute('id', 'back-top');
+div.style.display = 'none'
+
+
+let a = document.createElement('a');
+
+a.setAttribute('title', 'Go to Tap')
+a.setAttribute('href', '#')
+a.classList.add('smooth')
+a.style.color = 'white'
+a.innerHTML = '<i class="fas fa-level-up-alt"></i>'
+div.appendChild(a)
+
+document.querySelector('body').appendChild(div);
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > window.innerHeight - document.querySelector('header').clientHeight) {
+        div.style.display = 'block'
+
+    } else {
+        div.style.display = 'none'
+    }
+})
+
+document.querySelector('.smooth').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('header').scrollIntoView({
+        behavior: 'smooth'
+    });
 })
