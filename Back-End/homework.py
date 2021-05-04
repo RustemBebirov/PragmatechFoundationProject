9f288dc9e81272fc518d15650cc7698c8fe10c03
RustemBebirov <80380074+RustemBebirov@users.noreply.github.com>
2021-05-04 17:09:29 +0400

    Create homework.py

---
+++ Back-End/homework.py
@@ -0,0 +1,44 @@
+# home work1
+# hava_temperaturu=int(input('hava temperaturunu girin:'))
+
+# if(hava_temperaturu<=10):
+#     print('hava soyuq')
+# elif(10<hava_temperaturu<=20):
+#     print('hava normaldi')
+# elif(hava_temperaturu>=30):
+#     print('hava istidir')
+
+# home work2
+# ad='rustem'
+# soyad=' bebirov'
+# ad_soyad=ad+soyad
+# print(ad_soyad.title())
+
+
+# home work3
+# soz="proramlasdirma"
+
+# if 'x' in soz:
+#     print("yes")
+# else:
+#     print('No')
+
+
+# home work4
+# x=5
+# y=2
+# z=x**y
+# print(z)
+
+# home work5
+y='4.92'
+z=float(y)
+d=int(z)
+print(d)
+
+
+
+
+
+
+
