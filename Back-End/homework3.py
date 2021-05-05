fcd8d8ccd3a4c71f5c163817d4cecb3965890d64
RustemBebirov <80380074+RustemBebirov@users.noreply.github.com>
2021-05-06 01:44:47 +0400

    Update homework3.py

--- Back-End/homework3.py
+++ Back-End/homework3.py
@@ -29,3 +29,74 @@
 #             print('godbye')  
 # else:
 #     print('axtardiginiz melumat yoxudr')
+
+
+#home work5
+# x=[1,2,3]
+
+# if x==[]:
+#     print('empty')
+# else:
+#     for i in x:
+#         print(i)
+
+
+#home work 2
+# x=[1,5,500,10,100,80,100]
+
+# x.sort()
+
+# print(x[len(x)-1])
+
+
+#home work3
+
+# x=[1,5,500,10,0,100,80,100]
+
+# x.sort()
+
+# print(x[0])
+
+#home work 7
+
+# usernames=['admin','user','rustem','niyazi','orxan']
+# name=input('please enter your username')
+
+# if name in usernames:
+#     if name=='admin':
+#         print('Hello admin, would you like to see a status report? ')
+
+#     elif name=='rustem':
+#         print(' Hello Rustem, thank you for logging in again')
+#     else:
+#         print(f' Hello {name}, thank you for logging in again')
+
+
+
+# home work 6
+
+# mytuple = ("apple", "banana", "cherry","orange","mango")
+
+# for i in mytuple:
+#     print(i)
+# lst1=list(mytuple)
+
+
+# # replace=lst1.remove("apple")
+# replace1=lst1.pop(1)
+# x=lst1.index('cherry')
+# replace2=lst1.pop(x)
+
+# lst1.insert(0,'kivi')
+# lst1.append('ananas')
+
+# newtupple=tuple(lst1)
+# print(type(newtupple))
+
+# for i in newtupple:
+#     print(i)
+
+
+
+    
+
