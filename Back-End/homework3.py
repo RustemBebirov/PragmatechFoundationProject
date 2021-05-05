e070a6ae4851bd6e986eed5a49830fa90f5d6c0d
RustemBebirov <80380074+RustemBebirov@users.noreply.github.com>
2021-05-06 00:07:31 +0400

    Update homework3.py

---
+++ Back-End/homework3.py
@@ -0,0 +1,31 @@
+#home work8
+
+# db={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"} 
+
+# search=input('melumat daxil edin')
+
+# if search in db or 'what' in search or 'when' in search or 'where' :
+#     if 'when' in search or 'born' in search:
+#         cavab=input('Maybe did you mean “When was Plato born? yes or not')
+#         if cavab=='yes':
+#             name=db['name']
+#             born=db['born']
+#             print(f'{name} was born in {born}')
+#         else:
+#             print('godbye')
+#     elif 'name' in search or 'what' in search:
+#         cavab=input('Maybe did you mean What is her name? yer or not')
+#         if cavab=='yes':
+#             name=db['name']
+#             print(f'Her name is {name}')
+#         else:
+#             print('godbye')
+#     elif 'where' in search or 'country' in search:
+#         cavab=input('Maybe did you mean where is her country? yer or not')
+#         if cavab=='yes':
+#             country=db['country']
+#             print(f'Her country is {country}')
+#         else:
+#             print('godbye')  
+# else:
+#     print('axtardiginiz melumat yoxudr')
