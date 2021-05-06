2312da0b146063d210f17d73484941c66ccdc92c
RustemBebirov <80380074+RustemBebirov@users.noreply.github.com>
2021-05-06 23:00:52 +0400

    Create homework4.py

---
+++ Back-End/homework4.py
@@ -0,0 +1,45 @@
+# def cem( *args):
+#     toplam=0
+#     for i in args:
+#         toplam += i
+#     return toplam
+
+# print(cem(1,23,4,5))
+
+# def hasil(*args):
+#     vurma=1
+#     for i in args:
+#         vurma *=i
+#     return vurma
+
+# print(hasil(8,2,3,-1,7))
+        
+# def returnDay(a):
+#     if 1<=a<=7:
+#         if a==1:
+#             print('Sunday')
+#         elif a==2:
+#             print('Monday')
+#         elif a==3:
+#             print('Tuesdday')
+#         elif a==4:
+#             print('Wedisday')
+#         elif a==5:
+#             print("Thurday")
+#         elif a==6:
+#             print('Friday')
+#         elif a==7:
+#             print('Saturday')
+
+#     else:
+#         print('1 ile 7 arasinda reqem daxil edin')
+
+
+
+
+#home work 5
+# a=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+# 
+# for i in a:
+#     if i % 2 ==0 :
+#         print(i)
